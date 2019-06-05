3f3c993f7a85fd2a84d54f7c5870906b6213e9b8
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2019-06-05 08:37:35 +0530

    Small Changes

--- src/comp/cont.js
+++ src/comp/cont.js
@@ -13,22 +13,42 @@ class Content extends React.Component {
                         <div id="titl">
                             <div class="row">
                                 
-                                <span id="desg" class="col-md-7 col-lg-8">
+                                <span id="desg" class="col-md-6 col-lg-7">
                                     <h4>Full Stack Web | Android Developer</h4>
                                     <i id="sd"><b>Competent Programmer</b></i>
                                 </span>
-                                <span class="col-md-4 col-lg-3">
-                                <img id="im" src={me} class="img-rounded" ></img>
+                                
+                                <span class="col-sm-4 col-md-4 col-lg-3">
+                                <img id="im" src={me} class="img-rounded"></img>
                                 </span>
                             </div>
+                           
                             
                         </div>
                         
                     </div>
 
                 </div>
+               
+                <div id="story" class="row">
+               
+                     <span class="col-lg-2"></span>
+                     <div class="col-md-12 col-lg-8">
+                     
+                                <h4 id="on">Love to build stuff, be part of a great work and fulfill my life's purpose</h4>
+                                <br></br>
+                                <h5 id="tw">"Always a life long learner, believes that expertise comes through practical knowledge and by using them in real world scenerios"</h5>
+                                <h5 id="tw">"And always love to code, no matter what."</h5> 
+                     </div>
+                           
+                </div>
+                <br></br><br></br><br></br>
+                <div class="experience">
+                    Testing
+                </div>
             
             </div>
+            
         )
     }
 }
